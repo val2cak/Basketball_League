3aff9ac443ba7236fe83316974b4541a3d9b0c96
Valentina Tucak <62612785+val2cak@users.noreply.github.com>
2022-04-06 14:48:49 +0200

    cleaning up

--- client/src/pages/AboutUs/AboutUs.js
+++ client/src/pages/AboutUs/AboutUs.js
@@ -5,14 +5,7 @@ function AboutUs() {
      return (
           <div className="aboutUs">
                <label className="aboutUsLabel">KONTAKT</label>
-               <div className="aboutUsDiv">
-                    splitskakosarkaskaliga@gmail.com
-               </div>
                <label className="aboutUsLabel">LOKACIJA</label>
-               <div className="aboutUsDiv">
-                    Spaladium arena <br />
-                    Ul. Zrinsko Frankopanska 211, 21000, Split
-               </div>
           </div>
      );
 }
